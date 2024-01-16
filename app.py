6514a854e1dff827ca21e1bc7767c72e5cface81
Carter <carter.gamble014@gmail.com>
2024-01-16 07:16:27 -0700

    Start on app template

--- app.py
+++ app.py
@@ -2,8 +2,11 @@ from dash import Dash, html, dcc, callback, Input, Output, State
 
 app = Dash(__name__)
 
-app.layout = html.Div([
-    html.H1(children='Hello World')
+app.layout = html.Div(id="default-page", children=[
+    html.Div(id="title-bar", children=[
+        html.H1(id="app-name", children=dcc.Link('VisuCalc', href='/')),
+        html.H1(id="page-title", children='Home'),
+    ]),
 ])
 
 if __name__ == '__main__':
