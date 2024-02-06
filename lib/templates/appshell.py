3b68ae7ae5ae3c1db62151219a5fd03a787060c2
Carter <carter.gamble014@gmail.com>
2024-02-06 14:32:35 -0700

    Renaming files to follow naming convention

--- lib/templates/appshell.py
+++ lib/templates/appshell.py
@@ -189,7 +189,41 @@ def create_appshell():
                 )
             ]
         ),
-        theme={"colorScheme": "dark"},
+
+        theme={
+            "black": "#2d4b81",
+            "primaryColor": 'blue',
+            "styles": {"mantineProvider": {
+                "backgroundColor": "blue",
+            }},
+            "backgroundColor": "blue",
+            # add your colors
+            "colors": {
+                "deepBlue": [
+                    "#eef3ff",
+                    "#dce4f5",
+                    "#b9c7e2",
+                    "#94a8d0",
+                    "#748dc1",
+                    "#5f7cb8",
+                    "#5474b4",
+                    "#44639f",
+                    "#39588f",
+                    "#2d4b81"
+                ],  # 10 color elements
+            },
+            "shadows": {
+                # other shadows (xs, sm, lg) will be merged from default theme
+                "md": "1px 1px 3px rgba(0,0,0,.25)",
+                "xl": "5px 5px 3px rgba(0,0,0,.25)",
+            },
+            "headings": {
+                "fontFamily": "Roboto, sans-serif",
+                "sizes": {
+                    "h1": {"fontSize": 30},
+                },
+            },
+        },
         withGlobalStyles=True,
         withNormalizeCSS=True,
     )
@@ -212,4 +246,3 @@ def update_header(pathname):
 )
 def open_sb(opened):
     return opened
-
